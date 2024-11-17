c1b6b1e126122d188598e80d6ad1b82010af838c
gabe <132367056+GabrielHigino@users.noreply.github.com>
2024-11-17 00:25:09 -0300

    Update mdx-utils.js

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,18 @@
-import { api } from '../services/api'
-
-export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
-
-    if(data){
-        return data;
-    }
-
-    return []
-}
-
 export const getPostBySlug = async (id) => {
+    try {
+        // Fazendo a requisição para a API com o id fornecido
+        const { data } = await api.get(`/post?id=eq.${id}`);
+        
+        // Verifica se há dados e os retorna
+        if (data && data.length > 0) {
+            return data[0]; // Supondo que `id` seja único
+        }
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+        // Retorna um objeto vazio caso o post não seja encontrado
+        return {};
+    } catch (error) {
+        console.error('Erro ao buscar o post:', error);
+        // Retorna um objeto vazio em caso de erro
+        return {};
+    }
+};
